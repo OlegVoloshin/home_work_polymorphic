594ab9705141943c44b938b32be65904ed82833d
Oleg Voloshyn <sparco2405@gmail.com>
2020-03-31 21:41:46 +0300

    something wrong

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -69,6 +69,6 @@ class CommentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def comment_params
-      params.require(:comment).permit(:content)
+      params.require(:comment).permit(:content, :commentable_id, :commentable_type)
     end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def show
     @posts = Post.all
     @images = Image.all
+    @comments = Comment.all
   end
 end
