3a20eeaa24b74c78e8947be81499f05d06af349d
Oleg Voloshyn <sparco2405@gmail.com>
2020-03-31 15:22:56 +0300

    Add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/show'
   resources :comments
   resources :images
   resources :posts
+  root to: 'home#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
