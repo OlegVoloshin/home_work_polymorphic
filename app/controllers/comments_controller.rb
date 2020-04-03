2db168582e2da9933eaea3575c80aeda536adccb
Oleg Voloshyn <sparco2405@gmail.com>
2020-04-03 18:30:07 +0300

    Add redirecting to the show/post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,9 +26,10 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
 
+
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to @comment.commentable_id, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
